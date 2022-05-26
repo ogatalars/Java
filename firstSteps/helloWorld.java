eee87c0b806435affcb9d95c8f704b9ffd4ed59f
ogatalars <ogataogara@gmail.com>
2022-05-26 18:34:38 -0300

    primeira aula de java

---
+++ firstSteps/helloWorld.java
@@ -0,0 +1,7 @@
+// Public and class are two java keywords. The have a special meaning in java.
+//  The public Java Keyword is an access modifier. An access modifier allows us to define the scope or how other parts of your code or even some else's code ca access this code.
+// class keyword is used to define a class with the name following the keyword - hello in this case and left and right curly braces to define the class block
+public class Hello{
+
+}
+
